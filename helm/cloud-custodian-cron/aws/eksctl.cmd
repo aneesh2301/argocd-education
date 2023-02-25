fff7b052b4cd29d9efad638beff2034994eeb518
Sergey Dzyuban <sergey.dziuban@intapp.com>
2023-02-25 17:17:35 +0100

    update

--- helm/cloud-custodian-cron/aws/eksctl.cmd
+++ helm/cloud-custodian-cron/aws/eksctl.cmd
@@ -1,1 +1,4 @@
+
+eksctl utils associate-iam-oidc-provider --cluster karpenter --approve
 eksctl create iamserviceaccount --name c7n-service-account --namespace c7n --cluster karpenter --role-name "c7n-iam-role" --attach-policy-arn arn:aws:iam::154563028627:policy/c7n-iam-policy --approve
+update iamserviceaccount --name c7n-service-account --namespace c7n --cluster karpenter  --attach-policy-arn arn:aws:iam::154563028627:policy/c7n-iam-policy --approve
